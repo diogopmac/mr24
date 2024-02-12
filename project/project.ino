63e3f5d24482bab682b64c27f7c1918a2bf79a42
Maria Mota <mariamota04clf@gmail.com>
2024-02-12 19:17:48 +0000

    ==

--- project/project.ino
+++ project/project.ino
@@ -17,47 +17,47 @@ void loop() {
 
 void SeguePista()
 {
-  if (pista=4)
+  if (pista==4)
   {
     mDir(255, front);
     mEsq(255, front);
   }
-  if (pista=6)
+  if (pista==6)
   {
     mDir(150, front);
     mEsq(255, front);
   }
-  if (pista=2)
+  if (pista==2)
   {
     mDir(100, front);
     mEsq(255, front);
   }
-  if (pista=3)
+  if (pista==3)
   {
     mDir(0, front);
     mEsq(255, front);
   }
-  if (pista=1)
+  if (pista==1)
   {
     mDir(100, back);
     mEsq(255, front);
   }
-  if (pista=12)
+  if (pista==12)
   {
     mDir(255, front);
     mEsq(150, front);
   }
-  if (pista=8)
+  if (pista==8)
   {
     mDir(255, front);
     mEsq(100, front);
   }
-  if (pista=24)
+  if (pista==24)
   {
     mDir(255, front);
     mEsq(0, front);
   }
-  if (pista=16)
+  if (pista==16)
   {
     mDir(255, front);
     mEsq(100, back);
